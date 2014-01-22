eff0188e259f107b194266cda69bc6c5995f68c8
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2014-01-22 22:49:20 +0000

    Add tests for handler arguments

--- spec/walkObject-spec.js
+++ spec/walkObject-spec.js
@@ -1,5 +1,6 @@
 describe ( 'walker', function () {
     var _ = require ( 'underscore' );
+    var inspect = require ( 'eyes' ).inspector ();
     var walker = require ( __dirname + '/../objectWalker.js' );
     var objectWithAllTypes = require ( __dirname + '/testData.js' ).objectWithAllTypes;
 
@@ -64,7 +65,7 @@ describe ( 'walker', function () {
             walker.setCustomHandler ( 'Undefined', undefinedHandler.andReturn ( true ) );
         } );
 
-        it ( 'should walk the entire object with the built-in iterator', function () {
+        it ( 'should walk the entire object with the built-in iterator, and call all the value type handlers', function () {
             walker.walkObject ( objectWithAllTypes (), walker.iterator );
 
             expect ( arrayHandler ).toHaveBeenCalled ();
@@ -75,6 +76,61 @@ describe ( 'walker', function () {
             expect ( dateHandler ).toHaveBeenCalled ();
             expect ( nullHandler ).toHaveBeenCalled ();
             expect ( undefinedHandler ).toHaveBeenCalled ();
+        } );
+
+        it ( 'should walk the entire object with the built-in iterator, and call all the value type handlers with an array, and a value of the right type', function () {
+            walker.walkObject ( objectWithAllTypes (), walker.iterator );
+
+            expect ( arrayHandler ).toHaveBeenCalledWith ( jasmine.any ( Array ), jasmine.any ( Array ) );
+            expect ( objectHandler ).toHaveBeenCalledWith ( jasmine.any ( Array ), jasmine.any ( Object ) );
+            expect ( stringHandler ).toHaveBeenCalledWith ( jasmine.any ( Array ), jasmine.any ( String ) );
+            expect ( booleanHandler ).toHaveBeenCalledWith ( jasmine.any ( Array ), true );
+            expect ( numberHandler ).toHaveBeenCalledWith ( jasmine.any ( Array ), jasmine.any ( Number ) );
+            expect ( dateHandler ).toHaveBeenCalledWith ( jasmine.any ( Array ), jasmine.any ( Date ) );
+            expect ( nullHandler ).toHaveBeenCalledWith ( jasmine.any ( Array ), null );
+            expect ( undefinedHandler ).toHaveBeenCalledWith ( jasmine.any ( Array ), undefined );
+        } );
+    } );
+
+    describe ( 'customHandlers: called data', function () {
+        beforeEach ( function () {
+            var setHandler = function ( walker, type ) {
+                walker.setCustomHandler ( type, function ( keys, value ) {
+                    var i, possibleObjects = [ 'Object', 'Date', 'Null', 'Array' ];
+                    var valueInInput = objectWithAllTypes ();
+
+                    switch ( typeof ( value ) ) {
+                        case 'object':
+                            expect ( possibleObjects ).toContain ( type );
+                            break;
+                        default:
+                            expect ( type.toLowerCase () ).toBe ( typeof ( value ) );
+                    }
+
+                    for ( i = 0; i < keys.length; i++ ) {
+                        valueInInput = valueInInput[keys[i]];
+                    }
+
+                    expect ( value ).toEqual ( valueInInput );
+
+                    return true;
+                } );
+            };
+
+            setHandler ( walker, 'Array' );
+            setHandler ( walker, 'Object' );
+            setHandler ( walker, 'String' );
+            setHandler ( walker, 'Boolean' );
+            setHandler ( walker, 'Number' );
+            setHandler ( walker, 'Date' );
+            setHandler ( walker, 'Null' );
+            setHandler ( walker, 'Undefined' );
+        } );
+
+        it ( 'should walk the entire object with the built-in iterator, and call all the value type handlers with the list of keys and node value', function () {
+            var input = objectWithAllTypes ();
+
+            walker.walkObject ( input, walker.iterator );
         } );
     } );
 } );
